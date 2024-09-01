d870d1c3b932a922765f640eb1bef66fc9790419
Richard <z4nshin@gmail.com>
2024-09-01 15:32:56 +0200

    new commit for just qutebrowser

---
+++ userscripts/save_session.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+
+
+TEXT="$(zenity --entry --entry-text "Save session" --text="Save session")"
+
+#echo "open -t https://www.dict.cc/?s=$QUTE_SELECTED_TEXT" >> "$QUTE_FIFO"
+#echo "open -t https://www.dict.cc/?s=$TEXT" >> "$QUTE_FIFO"
+echo "session-save -o '$TEXT'" >> "$QUTE_FIFO"
+
